e47514c6508c35bf5e8e5ab9413299ed64c6307d
unknown <crichubiava@gmail.com>
2023-06-24 07:59:45 -0300

    Up Date 2023-06-24

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import numpy as np
 #import ast
 #from pandas.io.json import json_normalize
 #import calendar
+import locale
+locale.setlocale(locale.LC_ALL, 'es_ES.utf8')
+
+
+
 
 movies = pd.read_csv('Movies ETL.csv', sep=',') 
 credits = pd.read_csv('Credits ETL.csv', sep=',')
@@ -14,7 +19,7 @@ director = pd.read_csv('Director.csv', sep=',')
 app = FastAPI()
 
 @app.get("/")
-def titulo():
+def root():
     return {'Bienvenidos': 'al modelo de recomendación de películas'}
 
 @app.get("/cantidad_filmaciones_mes/{Mes}")
@@ -24,17 +29,18 @@ def cantidad_filmaciones_mes(Mes):
     # Convertir el mes a minúsculas y capitalizar la primera letra
     Mes = Mes.lower().capitalize()
 
-    #Cargo el DF
-    movies = pd.read_csv('Movies ETL.csv', sep=',')
-
     # Convertir la columna 'release_date' a tipo de dato datetime
     movies['release_date'] = pd.to_datetime(movies['release_date'])
 
     # Filtrar las películas que coinciden con el mes consultado. Para que reconozca los meses en Español tengo que usar el parámetro 'es_ES.utf8'.
-    filmaciones_mes = movies[movies['release_date'].dt.month_name(locale='es_ES.utf8') == Mes]
-    
+    #filmaciones_mes = movies[movies['release_date'].dt.month == Mes].release_date.count()
+    filmaciones_mes = movies[movies['release_date'].dt.month_name('es_ES.utf8') == Mes]
+
+
     # Obtener la cantidad de filmaciones en el mes
+    #cantidad = int(filmaciones_mes)
     cantidad = len(filmaciones_mes)
+
 
     return {'Mes': Mes, 'Cantidad de películas': cantidad}
     #f'{cantidad} cantidad de películas fueron estrenadas en el mes de {Mes}'
@@ -47,14 +53,11 @@ def cantidad_filmaciones_dia( Dia ):
     # Convertir el mes a minúsculas y capitalizar la primera letra
     Dia = Dia.lower().capitalize()
 
-    #Cargo el DF
-    movies = pd.read_csv('Movies ETL.csv', sep=',')
-
     # Convertir la columna 'release_date' a tipo de dato datetime
     movies['release_date'] = pd.to_datetime(movies['release_date'])
 
     # Filtrar las películas que coinciden con el mes consultado. Para que reconozca los meses en Español tengo que usar el parámetro 'es_ES.utf8'.
-    filmaciones_dia = movies[movies['release_date'].dt.day_name(locale='es_ES.utf8') == Dia]
+    filmaciones_dia = movies[movies['release_date'].dt.day_name('es_ES.utf8') == Dia]
     
     # Obtener la cantidad de filmaciones en el mes
     cantidad = len(filmaciones_dia)
@@ -63,54 +66,48 @@ def cantidad_filmaciones_dia( Dia ):
     #f'{cantidad} cantidad de películas fueron estrenadas en el mes de {Dia}'
 
 
-@app.get('/score_titulo/{titulo_de_la_filmación}')
-def score_titulo(titulo_de_la_filmación):
+@app.get("/score_titulo/{titulo_de_la_filmacion}")
+def score_titulo(titulo_de_la_filmacion: str):
 #Se ingresa el título de una filmación esperando como respuesta el título, el año de estreno y el score    
     #Paso a minúscula para que luego coincida con los valores del Dataframe.
-    titulo_de_la_filmación = titulo_de_la_filmación.lower()
-
-    #cargo el DF
-    movies = pd.read_csv("Movies ETL.csv", sep= ',')
+    titulo_de_la_filmacion = titulo_de_la_filmacion.lower()
 
     #En el df movies, pasar todos los títulos a minúsculas 
     movies['title'] =  movies['title'].str.lower()
 
     # Filtrar los datos por el título de la filmación
-    filtro = movies['title'] == titulo_de_la_filmación
+    filtro = movies['title'] == titulo_de_la_filmacion
     pelicula = movies.loc[filtro]
 
     # Verificar si se encontró alguna película con el título dado
     if pelicula.empty:
-        return "No se encontró ninguna película con ese título."
+        return {'Mensaje':"No se encontró ninguna película con ese título."}
 
     # Obtener el título, año de estreno y score de la película encontrada
-    titulo = pelicula['title'].values[0]
-    año_estreno = pelicula['release_year'].values[0]
-    score = pelicula['popularity'].values[0]    
+    titulo = str(pelicula['title'].iloc[0])
+    anio_estreno = str(pelicula['release_year'].iloc[0])
+    score = str(pelicula['popularity'].iloc[0])
 
-    return {'La película': titulo, 'fue estrenada en el año': año_estreno, 'con un score/popularidad de': score} 
-    #f"La película {titulo} fue estrenada en el año {año_estreno} con un score/popularidad de {score}."
+    return {'La película': titulo, 'fue estrenada en el año': anio_estreno, 'con un score/popularidad de': score}
+    #return f"La película {titulo} fue estrenada en el año {anio_estreno} con un score/popularidad de {score}."
 
 
-@app.get('/votos_titulo/{titulo_de_la_filmación}')
-def votos_titulo( titulo_de_la_filmación ):
+@app.get("/votos_titulo/{titulo_de_la_filmacion}")
+def votos_titulo( titulo_de_la_filmacion: str ):
 #Se ingresa el título de una filmación esperando como respuesta el título, la cantidad de votos y el valor promedio de las votaciones.
 #La misma variable deberá de contar con al menos 2000 valoraciones, 
 #caso contrario, debemos contar con un mensaje avisando que no cumple esta condición y que por ende, no se devuelve ningun valor.
 
 
     #Paso a minúscula para que luego coincida con los valores del Dataframe.
-    titulo_de_la_filmación = titulo_de_la_filmación.lower()
-
-    #cargo el DF
-    movies = pd.read_csv("Movies ETL.csv", sep= ',')
+    titulo_de_la_filmacion = titulo_de_la_filmacion.lower()
 
     #En el df movies, pasar todos los títulos a minúsculas 
     movies['title'] =  movies['title'].str.lower()
 
 
     # Filtrar por título de la filmación
-    filtro_titulo = movies['title'] == titulo_de_la_filmación
+    filtro_titulo = movies['title'] == titulo_de_la_filmacion
     pelicula = movies.loc[filtro_titulo]
 
     # Verificar la cantidad de votos
@@ -130,8 +127,8 @@ def votos_titulo( titulo_de_la_filmación ):
         #f'La película no cuenta con al menos 2000 valoraciones'
 
 
-@app.get('/get_actor/{nombre_actor}')
-def get_actor( nombre_actor ):
+@app.get("/get_actor/{nombre_actor}")
+def get_actor( nombre_actor: str):
 #Se ingresa el nombre de un actor debiendo devolver el éxito del mismo medido a través de la suma de los retornos obtenidos en todas sus películas. 
 #Además, debe devolver la cantidad de películas que en las que ha participado y el retorno promedio por película.
 
@@ -151,16 +148,16 @@ def get_actor( nombre_actor ):
     get_actor = get_actor[get_actor['cast_name'] == nombre_actor]
 
     #Hago los cálculos
-    count = get_actor['id'].count() #Cuento la cantidad de películas en la que participó el actor.
-    total_return = get_actor['return'].sum() #Éxito del actor, medido por la suma del retorno
-    avg_return = get_actor['return'].mean() #Se incluye también el retorno promedioen función de las películas en las que actuó.
+    count = str(get_actor['id'].count()) #Cuento la cantidad de películas en la que participó el actor.
+    total_return = str(get_actor['return'].sum()) #Éxito del actor, medido por la suma del retorno
+    avg_return = str(get_actor['return'].mean()) #Se incluye también el retorno promedioen función de las películas en las que actuó.
 
     return {'El actor': nombre_actor, 'ha participado de': count, 'filmaciones. Ha conseguido un retorno de': total_return, 'con un promedio por película de': avg_return} 
     #f'El actor {nombre_actor} ha participado de {count} cantidad de filmaciones, el mismo ha conseguido un retorno de {total_return} con un promedio de {avg_return} por filmación'
 
 
-@app.get('/get_actor/{nombre_director}')
-def get_director( nombre_director ):
+@app.get("/get_director/{nombre_director}")
+def get_director( nombre_director: str ):
 #Esta función pide com parámetro el nombre de un director.
 #Debe devolver el éxito del mismo medido como la suma de los retornos de todas las películas que dirigió.
 #Además, debe devolver el nombre de cada película que dirigió junto con su correspondiente fecha de lanzamiento (release_date), retorno individual (return), costo (bugdet) y ganancia (revenue)
@@ -184,16 +181,14 @@ def get_director( nombre_director ):
     dir_total_return = get_director['return'].sum()
 
     #Agrego al df la columna con el total_return
-    get_director['director_total_return'] = get_director['return'].sum()
+    #get_director['director_total_return'] = get_director['return'].sum()
 
     #Devuelvo el return total y una tabla con el detalle de cada película
-    return get_director
+    #return get_director
 
-        #(
-        #f"El director {nombre_director} ha conseguido un retorno total de {dir_total_return}.",
-        #f"A continuación se muestra un detalle de todas las películas que digrigió:",
-        #get_director
-        #)
+    return {"El director": nombre_director, 'ha conseguido un retorno total de': dir_total_return, 
+            "A continuación se muestra un detalle de todas las películas que digrigió:": get_director}
+
 
 
 
