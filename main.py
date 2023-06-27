05ba22ed01918d51db00e98c6de7be3abe9c69b9
unknown <crichubiava@gmail.com>
2023-06-26 21:33:03 -0300

    Up Date 2023-06-26

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pandas as pd
 import numpy as np
+import sklearn
 #import seaborn as sns
 from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.metrics.pairwise import cosine_similarity
